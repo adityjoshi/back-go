d111f7283dd3b74fd84e25038bb5b39dea6d24dc
Aditya Joshi <aditya03joshi@gmail.com>
2024-03-18 22:27:51 +0530

    go

--- routes/userRoutes.go
+++ routes/userRoutes.go
@@ -9,12 +9,27 @@ func UserRoute(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("/login")
 	incomingRoutes.GET("/warden", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"hey": "i am a function",
+			"message": "This function is accessible by wardens only",
 		})
 		c.Next()
 	})
-	incomingRoutes.GET("/worker")
-	incomingRoutes.GET("/student")
-	incomingRoutes.GET("/complaint")
+	incomingRoutes.GET("/worker", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "This function is accessible by worker only",
+		})
+		c.Next()
+	})
+	incomingRoutes.GET("/student", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "This function is accessible by students only",
+		})
+		c.Next()
+	})
+	incomingRoutes.GET("/complaint", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "This function is accessible by complaint only",
+		})
+		c.Next()
+	})
 
 }
